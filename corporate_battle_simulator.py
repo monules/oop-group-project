08beaf8645eebe13971c1daeadecf32c74004adf
syulcherr <156232164+syulcherr@users.noreply.github.com>
2025-11-27 20:28:07 +0900

    Update corporate_battle_simulator.py
    healing_break mechanic added, ideas of skill and their possible effect, contract & task subclasses added, i tried something in the intern class

--- corporate_battle_simulator.py
+++ corporate_battle_simulator.py
@@ -7,6 +7,18 @@ import random
 ### employee is the player & associated attributes, contract is the main quest, task is like side-quest to up xp & gain skills ###
 ### some getter & setter methods are missing ###
 
+### list of skills in the game :
+## Organisation : reduce stress impact, reduce coffee consumption
+## Adaptability : reduce stress impact
+## Teamwork : increase motivation
+## Problem Solving : increase chance of success
+## Communication : increase chance of success
+## Leadership : increase motivation, increase stress_level
+## Creativity : increase chance of success
+## Critical Thinking : increase chance of success
+## Negotiation : increase chance of success
+
+
 class Employee(ABC):
     def __init__(self,name,role,xp,motivation_level,stress_level,coffee_level,skills):
         self.name = name
@@ -25,12 +37,36 @@ class Employee(ABC):
     def work_task(self,task):
         pass
     
-    # maybe we can make the reduction & increase values dependant of the level of the attributes ?
+    # reduction/increase values being dependant on current levels adds an intelligent mechaanic
     @abstractmethod
-    def healing_break(self):
-        stress_reduction = 20
-        motivation_increase = 15
-        coffee_increase = 10
+    def healing_break(self,player):
+        if player.stress_level <= 25:
+            stress_reduction = 5
+        elif player.stres_level <= 50:
+            stress_reduction = 10
+        elif player.stress_level <= 75:
+            stress_reduction = 20
+        else:
+            stress_reduction = 25
+        
+        if player.motivation_level <= 25:
+            motivation_increase = 5
+        elif player.motivation_level <= 50:
+            motivation_increase = 10
+        elif player.motivation_level <= 75:
+            motivation_increase = 20
+        else:
+            motivation_increase = 25
+        
+        if player.coffee_level <= 25:
+            coffee_increase = 5
+        elif player.coffee_level <= 50:
+            coffee_increase = 10
+        elif player.coffee_level <= 75:
+            coffee_increase = 20
+        else:
+            coffee_increase = 25
+        
         self._stress_level = max(0, self._stress_level - stress_reduction)
         self._motivation_level = min(100, self._motivation_level + motivation_increase)
         self._coffee_level = min(100, self._coffee_level + coffee_increase)
@@ -177,7 +213,23 @@ class Task(ABC):
     
 class Intern(Employee):
     def __init__(self,name,role,xp,motivation_level,stress_level,coffee_level,skills):
-        super().__init__(name,role,xp,motivation_level,stress_level,coffee_level,skills)
+        if skills is None:
+            skills = []
+        super().__init__(name, "Intern", xp, motivation_level, stress_level, coffee_level, skills)
+        self._promotion_xp = 100
+
+    def work_contract(self,contract):
+        return super().work_contract(contract)
+    
+    def work_task(self,task):
+        return super().work_task(task)
+    
+    def healing_break(self):
+        return super().healing_break()
+    
+    def check_status(self):
+        return super().check_status()
+    
     
 
 class Junior(Employee):
@@ -208,8 +260,99 @@ class President(Employee):
 
 ### Classes inheriting from Contract ###
 
+class SmallContract(Contract):
+    def __init__(self,name,xp,required_skills,stress_tool):
+        super().__init__(name,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_contract(self)
+
+class MediumContract(Contract):
+    def __init__(self,name,xp,required_skills,stress_tool):
+        super().__init__(name,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_contract(self)
+
+class LargeContract(Contract):
+    def __init__(self,name,xp,required_skills,stress_tool):
+        super().__init__(name,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_contract(self)
+
+class CriticalContract(Contract):
+    def __init__(self,name,xp,required_skills,stress_tool):
+        super().__init__(name,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_contract(self)
+
 
 ### Classes inheriting from Task ###
+
+class InitiationTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class MailSendingTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class ReportFilingTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class PresentationTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class ClientMeetingTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class ProjectManagementTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class ForecastingTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class StrategicPlanningTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
+
+class SchedulingTask(Task):
+    def __init__(self,name,description,xp,required_skills,stress_tool):
+        super().__init__(name,description,xp,required_skills,stress_tool)
+    
+    def complete(self,employee):
+        return employee.work_task(self)
 
 
 ### Game controller ###
@@ -226,7 +369,7 @@ class GameController:
         self.tasks = []      # Load or define initial tasks
     
     def create_player(self, name):
-        self.player = Intern(name)
+        self.player = Intern(f"{name}", "Intern", 0, 50, 20, 50, [])
         return self.player
     
     ## add a saving thing for the game pls
@@ -259,7 +402,8 @@ def main():
         elif choice == 2:
             pass
         elif choice == 3:
-            pass
+            heal = player.healing_break()
+            print(heal)
         elif choice == 4:
             status = player.check_status()
             print(status)
